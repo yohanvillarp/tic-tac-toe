6044282c9b0a0ec1d2b34f7bb144158293adf76f
Yohan Nikel Villar Presentación <yohannikel21@gmail.com>
2025-06-26 11:12:42 -0500

    Tutorial seguido

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Square from "./components/Square";
+import React, { useState } from "react";
+import Game from "./components/Game";
+
+export default function Board() {
+return (
+  <>
+    <Game />
+  </>
+)
+}
---
+++ src/components/Game.js
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import Board from './Board';
+
+export default function Game() {
+
+    const [history, setHistory] = useState([Array(9).fill(null)]);
+    const [currentMove, setCurrentMove] = useState(0);
+    const xIsNext = currentMove % 2 === 0;
+    const currentSquares = history[currentMove];
+
+    function handlePlay(nextSquares){
+        const nextHistory = [...history.slice(0, currentMove+1), nextSquares];
+        setHistory(nextHistory);
+        setCurrentMove(nextHistory.length-1);
+    }
+
+    function jumpTo(nextMove){
+        setCurrentMove(nextMove);
+    }
+
+    const moves = history.map((squares, move) =>{
+        let description;
+        if(move > 0){
+            description="Ir al movimiento #" + move;
+        }else{
+            description="Ir al inicio del juego";
+        }
+        return (
+            <li key={move}>
+                <button onClick={() => jumpTo(move)}>{description} </button>
+            </li>
+        )
+    })
+
+    return (
+        <div className="game">
+            <div className="game-board">
+                <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
+            </div>
+            <div className="game-info">
+                <ol>{moves}</ol>
+            </div>
+        </div>
+    );
+}
